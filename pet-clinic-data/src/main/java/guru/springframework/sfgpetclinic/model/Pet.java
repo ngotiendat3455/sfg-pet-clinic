89927e6bff8bd8c49fd2f5a7dc03e7772cc634e0
datnt <datngotien345@gmail.com>
2023-08-20 21:39:19 +0700

    added visit entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,9 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -15,6 +18,8 @@ public class Pet extends BaseEntity {
     private LocalDate birthDate;
     @Column(name = "name")
     private String name;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
